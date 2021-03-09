99738eb771c94b49275e4689cc1025547537b6a1
blackbirdx32 <samuelsobreira12@gmail.com>
2021-03-09 16:39:24 -0300

    quarto commit

--- php/nota.php
+++ php/nota.php
@@ -21,41 +21,61 @@
             <div class="imagemNota">
                 <img src="..\imagens\<?php echo $row['imagem'];?>">
             </div>
-
+            
             <div class="textoNota">
                 <h1><?php echo $row['titulo'];?></h1>
                 <p><?php echo $row['resumo'];?></p>
                 <p class="justificado"><?php echo $row['texto'];?></p>
-                <form method="POST" action="alterar_form.php">
+            </div>
+
+            <nav class="opc">
+                <form method="POST" action="alterarForm.php">
                     <input type="hidden" name="id" value="<?php echo $row['id'];?>">
                     <input type="submit" value="Alterar">
                 </form>
+
                 <form method="POST" action="deletar.php">
                     <input type="hidden" name="id" value="<?php echo $row['id'];?>">
                     <input class="vermelho" type="submit" value="Excluir">
                 </form>
-            </div>
+            </nav>
 
             <div class="linha"></div>
         </section>
-
+        
         <section class="bloco">
-                <div class="topico">
-                    <img src="..\imagens\blackbird.jpg">
-                </div>
-                <div class="topico">
-                    <img src="..\imagens\caligari.jpg">
-                </div>
-                <div class="topico">
-                    <img src="..\imagens\nosferatu.jpg">
-                </div>
-        </section>
-        <?php
-            }
-            mysqli_close($con);
-        ?>      
+                <?php
+                    $sql1 = "SELECT * FROM topico";
+                    $result1 = mysqli_query($con,$sql1);
+        
+                    while($row1 = mysqli_fetch_array($result1)){
+                        if ($row1['nota_id'] == $row['id']){
+                ?>
 
+                <div class="topico">
+                    <img src="..\imagens\<?php echo $row1 ['imagem'];?>">
+                    <form method="POST" action="topico.php">
+                        <input type="hidden" name="id" value="<?php echo $row1['id'];?>">
+                        <button type="submit"><img src="..\imagens\olho.png"></button>
+                    </form>
+                </div>
+                
+                <?php
+                                }
+                        }
+                    }
+                    mysqli_close($con);
+                ?> 
+
+                <div class="botaoTopico">
+                    <a href="inserirTopicoForm.php"><img src="..\imagens\cruz.png" ></a>
+                </div>
+                <div class="linha"></div>
+        </section>
+             
+        
         <div class="botao">
+            
             <a href="..\index.php"><img src="..\imagens\boc.jpg" ></a>
         </div>
         
